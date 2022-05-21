2470b9095adc00489c97262d7ee57ed7a676fd0a
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-05-21 05:52:44 +0200

    update nv-pasenporno_mp3.js

--- plugins/nv-pasenporno_mp3.js
+++ plugins/nv-pasenporno_mp3.js
@@ -5,8 +5,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/pasen-porno.mp3'
-conn.sendFile(m.chat, vn, 'pasen-porno.mp3', null, m, true, {
+let vn = './media/pasenporno.mp3'
+conn.sendFile(m.chat, vn, 'pasenporno.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
