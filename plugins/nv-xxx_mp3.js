12b1f9e56595bdc20d4ac03d3acfafa506c1f7f0
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-05-21 06:06:15 +0200

    update nv-xxx_mp3.js

--- plugins/nv-xxx_mp3.js
+++ plugins/nv-xxx_mp3.js
@@ -1,1 +1,16 @@
+import util from 'util'
 
+import path from 'path'
+
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/xxx.mp3'
+conn.sendFile(m.chat, vn, 'xxx.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /Pasen porno|Xxx/
+handler.command = new RegExp
+export default handler
