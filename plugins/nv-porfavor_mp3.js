ee1af7b5f7d1664f05d1cb80e147a44ae3b643dc
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-05-29 20:09:29 +0200

    update nv-porfavor_mp3.js

--- plugins/nv-porfavor_mp3.js
+++ plugins/nv-porfavor_mp3.js
@@ -1,16 +1,18 @@
-import util from 'util' 
- import path from 'path' 
-  
- let handler = async (m, { conn }) => { 
- if (!db.data.chats[m.chat].audios && m.isGroup) throw 0 
- let vn = './media/porfavor.mp3' 
- conn.sendFile(m.chat, vn, 'porfavor.mp3', null, m, true, { 
- type: 'audioMessage',  
- ptt: true  
- }) 
- } 
- handler.customPrefix = /Por favor|por favor/porfa/
- handler.command = new RegExp 
- handler.fail = null 
- handler.exp = 100 
- export default handler
+import util from 'util'
+
+import path from 'path'
+
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/porfavor.mp3'
+conn.sendFile(m.chat, vn, 'porfavor.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /porfavor|Por favor|por favor|porfis|Porfis|Porfa/
+handler.command = new RegExp
+handler.fail = null
+handler.exp = 100
+export default handler
