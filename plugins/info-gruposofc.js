9e61930a108ab67e7a8eb5b4759594d8e675a945
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-08-02 18:57:13 +0200

    update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *➤ Grupos oficiales del Bot:*
 *1.-* https://chat.whatsapp.com/KvgaVzx3iET6KGfPCpI84U
 
-*2.-* https://chat.whatsapp.com/HDXzLnYB8WJ6ErstgerZFp
+*2.-* https://chat.whatsapp.com/IfIS42piGFV8srivZDDe8M
 
 `.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
 handler.command = /^linkgc|grupos$/i
